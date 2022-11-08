1da622bcd62839c715c84b575c3d3d3802acc037
sxrose <sxrose1.o@gmail.com>
2022-11-08 14:07:17 +0700

    cursed formating

--- Python/pack4/mini9.py
+++ Python/pack4/mini9.py
@@ -1,36 +1,41 @@
 from typing import List
 
+
 def get_format_row(words: List[str], column_lens: List[int]) -> str:
     assert len(words) == len(column_lens)
-    
+
     return "| " + " | ".join([f"{w:<{l}}" for w, l in zip(words, column_lens)]) + " |"
+
 
 def format_table(benchmarks: List[str], algos: List[str], results: List[List[float]]):
     BENCHMARKS: str = "Benchmarks"
-    results_str: List[str] = [list(map(lambda n: str(round(n, 2)), seq)) for seq in results]
+    results_str: List[str] = [
+        list(map(lambda n: str(round(n, 2)), seq)) for seq in results]
 
     column_lens: List[int] = list(
         map(
-            lambda xs: max(map(len, xs)), 
-
-            [[BENCHMARKS] + benchmarks] + 
-            [[col_name] + [results_str[j][i] for j in range(len(results_str))] 
-                for i, col_name in enumerate(algos)]
-        )    
+            lambda xs: max(map(len, xs)),
+            [[BENCHMARKS] + benchmarks] + [
+                [col_name] + [
+                    results_str[j][i] for j in range(len(results_str))] 
+                    for i, col_name in enumerate(algos)])
     )
 
     print(
         "\n".join(
-            [get_format_row([BENCHMARKS] + algos, column_lens), 
-                "|" + "-" * (sum(column_lens) + 3 * len(column_lens) - 1) + "|"] + 
-                
-            [get_format_row([bench] + res, column_lens) for bench, res in zip(benchmarks, results_str)]
+            [get_format_row([BENCHMARKS] + algos, column_lens),
+             "|" + "-" * (sum(column_lens) + 3 * len(column_lens) - 1) + "|"] +
+
+            [get_format_row([bench] + res, column_lens)
+             for bench, res in zip(benchmarks, results_str)]
         )
     )
-## surprisingly wackiest thing i ever wrote
-    
+
+
+# surprisingly wackiest thing i have wrote thus far
+
 format_table(
-    ["best case", "the worst case"], 
-    ["quick sort", "merge sort", "bubble sort"], 
+    ["best case", "the worst case"],
+    ["quick sort", "merge sort", "bubble sort"],
     [[1.23, 1.56, 2.0], [3.3, 2.9, 3.9]]
-    )
+)
