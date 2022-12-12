54f260dc75a20de22c6b72adb652a9d848e34b9d
sxrose <sxrose1.o@gmail.com>
2022-12-13 00:16:22 +0700

    add solution to mini 13

---
+++ Python/pack7/mini13.py
@@ -0,0 +1,12 @@
+from typing import List, Iterable
+
+def chain(*iters: List[Iterable]):
+    for i in iters:
+        yield from i
+
+
+if __name__ == "__main__":
+    c = chain(range(10), [420, 69], (1337,))
+    expected = list(range(10)) + [420, 69, 1337]
+
+    assert list(c) == expected
