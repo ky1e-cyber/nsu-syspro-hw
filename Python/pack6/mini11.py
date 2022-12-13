fe03df541505ebf60222d339c4c4d0435ab3a63f
sxrose <sxrose1.o@gmail.com>
2022-12-13 11:39:27 +0700

    add init

--- Python/pack6/mini11.py
+++ Python/pack6/mini11.py
@@ -4,6 +4,13 @@ class Singleton:
 
     instance = None
 
+    def _init_make(self, *args, **kwargs):
+        super().__init__(self, *args, **kwargs)
+        Singleton.__init__ = Singleton._init_get
+
+    def _init_get(self, *args, **kwargs):
+        pass
+
     def _new_make(cls, *args, **kwargs):
         Singleton.instance = super().__new__(cls, *args, **kwargs)
         Singleton.__new__ = Singleton._new_get
@@ -15,9 +22,13 @@ class Singleton:
     def __new__(cls, *args, **kwargs):
         return Singleton._new_make(cls, *args, **kwargs)
 
+    def __init__(self, *args, **kwargs):
+        Singleton._init_make(self, *args, **kwargs)
+
     def __del__(self):
         Singleton.instance = None
         Singleton.__new__ = Singleton._new_make
+        Singleton.__init__ = Singleton._init_make
 
 if __name__ == "__main__":
     from collections import UserList
@@ -26,10 +37,10 @@ if __name__ == "__main__":
         pass
 
     l1 = LameList()
-    l2 = LameList()
-
-    assert id(l1) == id(l2)
 
     l1.extend([1, 2, 3])
 
+    l2 = LameList()
+
+    assert id(l1) == id(l2)
     assert l1 == l2
