614afbcfa9b5dbbd7a0bb98e7a6579b22422171b
sxrose <sxrose1.o@gmail.com>
2023-04-06 00:41:50 +0700

    qfix for mini17

--- Algorithms/pack6/mini17.py
+++ Algorithms/pack6/mini17.py
@@ -7,17 +7,24 @@ class ListNode:
         self.next = next
 class Solution:
     def reverseBetween(self, head: Optional[ListNode], left: int, right: int) -> Optional[ListNode]:
+
+        if head.next == None:
+            return head
+
         def reverse_slice(slice_len) -> Tuple[ListNode, ListNode]:
             nonlocal curr
             new_tail = curr
             buff = None
+            nxt = curr.next
             for _ in range(slice_len - 1):
                 buff = curr
-                curr = curr.next
+                curr = nxt
+
+                nxt = curr.next
                 curr.next = buff
 
             new_head = curr
-            curr = curr.next
+            curr = nxt
             return new_head, new_tail 
 
         curr = head
@@ -25,12 +32,18 @@ class Solution:
         for _ in range(left - 2):
             curr = curr.next
         
-        prev = curr
-        curr = curr.next
+        prev = None
+        if left != 1:
+            prev = curr
+            curr = curr.next
 
         new_head, new_tail = reverse_slice(right - left + 1)
         
-        prev.next = new_head
         new_tail.next = curr
+
+        if prev:
+            prev.next = new_head
+        else:
+            head = new_head
 
         return head
