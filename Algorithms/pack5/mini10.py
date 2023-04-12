736a2178d242af355aeb86e633b6e4f8f05b96b1
sxrose <sxrose1.o@gmail.com>
2023-04-12 16:51:22 +0700

    replace magic number with named constant

--- Algorithms/pack5/mini10.py
+++ Algorithms/pack5/mini10.py
@@ -1,12 +1,15 @@
 from typing import List
 
+## ASCII Characters 
+CHAR_SET_SIZE = 128
+
 def str_radix_sort(strings: List[str]) -> List[str]:
     def chr_counting_sort(ind: int) -> List[str]:
-        count_list = [0] * 128
+        count_list = [0] * CHAR_SET_SIZE
         for s in strings:
             count_list[ord(s[ind])] += 1
 
-        for i in range(1, 128):
+        for i in range(1, CHAR_SET_SIZE):
             count_list[i] += count_list[i - 1]
 
         ret = [None] * len(strings)
