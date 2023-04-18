22f6c36fc3363a0de1df9fa13d40ea883ea54ab4
sxrose <sxrose1.o@gmail.com>
2023-04-18 23:42:47 +0700

    wip in mini19

--- Algorithms/pack7/mini19.py
+++ Algorithms/pack7/mini19.py
@@ -1,15 +1,26 @@
-from collections import namedtuple
-from typing import Optional, List
+from typing import Optional, List, Any
+from dataclasses import dataclass
 
 # Definition for singly-linked list.
 class ListNode:
     def __init__(self, val=0, next=None):
         self.val = val
         self.next = next
+    def __str__(self) -> str:
+        h = self
+        l = []
+        while h:
+            l.append(h.val)
+            h = h.next
+
+        return "[" + " ".join(map(str, l)) + "]"
 
 class BinaryHeap:
 
-    Node = namedtuple("Node", ["key", "value"])
+    @dataclass
+    class Node:
+        key: int
+        value: Any
 
     def __init__(self):
         self._list: List[BinaryHeap.Node] = []
@@ -19,27 +30,31 @@ class BinaryHeap:
             self._list[ind2], self._list[ind1] 
 
     def get_min(self):
-        return self._list[0].value
+        return (
+            self._list[0].value if self._list
+            else None
+        )
     
     def _get_parrent(self, ind: int) -> int:
         if ind == 0:
             return -1
         return (ind - 1) // 2
     
-    def _increase_key(self, ind: int, new_key: int):
+    ## TODO:
+    def increase_key(self, ind: int, new_key: int):
         def max_ind(i):
-            if len(self._list) < (2 * i):
+            if len(self._list) < (2 * i) + 2:
                 return -1
 
-            if len(self._list) == (2 * i):
-                return (2 * i)
+            if len(self._list)  == (2 * i) + 2:
+                return 2 * i + 1
 
             left = self._list[(2 * i + 1)]
             right = self._list[(2 * i + 2)]
             
             return (
                 (2 * i + 1) if left.key > right.key 
-                else (2 * i + 1)
+                else (2 * i + 2)
             )
         
         self._list[ind].key = new_key
@@ -54,7 +69,7 @@ class BinaryHeap:
             child_ind = max_ind(new_ind)
 
 
-    def _decrease_key(self, ind: int, new_key: int):
+    def decrease_key(self, ind: int, new_key: int):
         parr_ind = self._get_parrent(ind)
 
         self._list[ind].key = new_key
@@ -66,21 +81,24 @@ class BinaryHeap:
 
     def add(self, key, value):
         self._list.append(BinaryHeap.Node(key, value))
-        self._decrease_key(len(self._list ) - 1, key)
+        self.decrease_key(len(self._list ) - 1, key)
 
     def pop(self):
-
         if len(self._list) == 0:
             return None
+
+        if len(self._list) == 1:
+            return self._list.pop().value
 
         res = self._list[0].value
 
         self._swap_nodes(0, len(self._list) - 1)
         self._list.pop()
-        self._increase_key(0, self._list[0].key)
+        self.increase_key(0, self._list[0].key)
 
         return res
-    
+
+
 class Solution:
     def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
         beap = BinaryHeap()
@@ -88,9 +106,49 @@ class Solution:
         for lst in lists:
             if lst:
                 beap.add(lst.val, lst)
-        
-        nxt_node = beap.get_min()
 
-        while 
+        curr_node = beap.get_min()
 
+        res = ListNode(val=curr_node.val) 
+        res_head = res
+    
+
+        while True:
+            if curr_node.next is None:
+                beap.pop()
+            else:
+                beap._list[0].value = curr_node.next
+                beap.increase_key(
+                    0, 
+                    beap._list[0]
+                        .value
+                        .val
+                )
+
+            curr_node = beap.get_min()
+
+            if curr_node is None:
+                break
+
+            res.next = ListNode(val=curr_node.val)
+            res = res.next
+
+        return res_head
+
+
+s = Solution()
+
+l1 = ListNode(val=1)
+l1.next = ListNode(val=4)
+l1.next.next = ListNode(val=5)
+
+
+l2 = ListNode(val=1)
+l2.next = ListNode(val=3)
+l2.next.next = ListNode(val=4)
+
+l3 = ListNode(val=6)
+l3.next = ListNode(val=7)
+
+print(s.mergeKLists([l1, l2, l3]))
 
