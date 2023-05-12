84920888efaa36c74ebd0d490cb579b1e973df3c
sxrose <sxrose1.o@gmail.com>
2023-05-12 20:41:12 +0700

    fixes for pack8 x2

--- Algorithms/pack8/mini22.py
+++ Algorithms/pack8/mini22.py
@@ -6,44 +6,25 @@ class TreeNode:
         self.val = val
         self.left = left
         self.right = right
+
 class Solution:
     def rightSideView(self, root: Optional[TreeNode]) -> List[int]:
-        def get_subtree(rt: TreeNode, curr_height) -> Optional[TreeNode]:
-            local_height = 0
 
-            while local_height <= curr_height:
-                if rt.right is not None:
-                    rt = rt.right
-                elif rt.left is not None:
-                    rt = rt.left
-                else:
-                    return None
-                local_height += 1
-            
-            return rt
-            
-
-        def put_subtree(rt: Optional[TreeNode], height: int) -> int:
-            if rt == None:
-                return height
-            
-
-
-        def put_subtree(rt: Optional[TreeNode]):
+        def put(rt: Optional[TreeNode], depth: int):
+            nonlocal covert_depth
             if rt == None:
                 return
-            
-            lst.append(rt.val)
-            curr_height += 1
 
-            nxt = get_subtree(rt)
+            if depth > covert_depth:
+                covert_depth = depth
+                lst.append(rt.val)
 
-            if nxt is None:
-                return
-            
-            curr_height += 1
-            lst.append(rt.val)
-            put_subtree(rt.right)
-            
+            put(rt.right, depth + 1)
+            put(rt.left, depth + 1)
 
         lst = []
+        covert_depth = 0
+
+        put(root, 1)
+
+        return lst
