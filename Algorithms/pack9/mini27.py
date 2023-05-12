4495fb9f7fdbfc46ee7c7e6c41928d3df6a40bea
sxrose <sxrose1.o@gmail.com>
2023-05-12 23:14:35 +0700

    properly add mini27

--- Algorithms/pack9/mini27.py
+++ Algorithms/pack9/mini27.py
@@ -1,18 +1,19 @@
-
 from functools import reduce
-from typing import Set
-
+from typing import Set, List
 
 class Solution:
     def findRepeatedDnaSequences(self, s: str) -> List[str]:
-        s = set()
+        encounterd = set()
 
         def f(acc_set: Set[str], dna_seq: str):
-            if dna_seq in s:
+            if dna_seq in encounterd:
                 acc_set.add(dna_seq)
+            else:
+                encounterd.add(dna_seq)
             return acc_set
             
-        def gen(dna: str):
+        def get_dna_seq():
+            for i in range(9, len(s)):
+                yield s[i - 9:i + 1:]
 
-        
-        return list(reduce(f, ))
+        return list(reduce(f, get_dna_seq(), set()))
